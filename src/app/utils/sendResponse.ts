3d7bfb296d646716c57c5208ec03db2b95fe0c42
ShariarNiaj05 <shariarn85@gmail.com>
2024-06-12 22:39:57 +0600

    user model and error response updated

--- src/app/utils/sendResponse.ts
+++ src/app/utils/sendResponse.ts
@@ -1,19 +1,29 @@
 import { Response } from 'express'
+import { JwtPayload } from 'jsonwebtoken'
 
 type TResponse<T> = {
   statusCode: number
   success: boolean
   message?: string
+  token?: JwtPayload
   data: T
 }
 
 const sendResponse = <T>(res: Response, data: TResponse<T>) => {
-  res.status(data?.statusCode).json({
-    success: data.success,
-    statusCode: data?.statusCode,
-    message: data.message,
-    data: data.data,
-  })
+  if (!data.data) {
+    res.status(data?.statusCode).json({
+      success: false,
+      message: 'No Data Found',
+      data: [],
+    })
+  } else {
+    res.status(data?.statusCode).json({
+      success: data.success,
+      statusCode: data?.statusCode,
+      message: data.message,
+      data: data.data,
+    })
+  }
 }
 
 export default sendResponse
