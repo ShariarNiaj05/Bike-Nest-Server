60ec868c368caa7cb4a4c3abe927121f4dd2cffd
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-06 16:30:12 +0600

    bike interface updated

--- src/app/modules/bike/bike.interface.ts
+++ src/app/modules/bike/bike.interface.ts
@@ -2,6 +2,7 @@ export type TBike = {
   name: string
   description: string
   pricePerHour: number
+  imageUrl: string
   isAvailable?: boolean // optional, defaults to true if not provided
   cc: number
   year: number
