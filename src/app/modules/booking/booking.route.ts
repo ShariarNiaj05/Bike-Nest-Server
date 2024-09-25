16bae147d0f753ca5978b5cfa4a9c309c8fd0b89
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-26 01:19:50 +0600

    feat: return bike

--- src/app/modules/booking/booking.route.ts
+++ src/app/modules/booking/booking.route.ts
@@ -6,7 +6,7 @@ import { USER_ROLE } from '../user/user.constant'
 const router = Router()
 
 router.post('/', auth(USER_ROLE.user), BookingControllers.createRental)
-router.get('/', auth(USER_ROLE.user), BookingControllers.getAllRentalsForUser)
+router.get('/', BookingControllers.getAllRentalsForUser)
 router.put('/:id/return', auth(USER_ROLE.admin), BookingControllers.returnBike)
 
 export const BookingRoutes = router
