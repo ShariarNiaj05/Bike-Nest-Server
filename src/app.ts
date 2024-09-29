02327f0b4e0dfe107abe3a9f05de52361716e62e
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-30 00:24:24 +0600

    deployed

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,12 @@ const app: Application = express()
 
 //parsers
 app.use(express.json())
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }))
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://bike-nest-client.vercel.app'],
+    credentials: true,
+  }),
+)
 app.use(cookieParser())
 
 // application routes
