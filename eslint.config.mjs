9165266353d95cb210b8128402d98bf697fb82c1
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-29 23:35:28 +0600

    fix: multiple lint error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,6 +25,7 @@ export default [
     },
     rules: {
       'no-unused-vars': 'warn',
+      'no-explicit-any': 'warn',
       semi: ['error', 'always'],
       'no-console': 'warn',
       'no-unused-expressions': 'error',
